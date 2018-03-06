a1a3da2ae439ab32bc12c2b0daaf74d05a3527c2
el Puas <elpuas@gmail.com>
2018-03-06 15:35:47 -0500

    Deploy HomePage to Production

--- functions.php
+++ functions.php
@@ -4,5 +4,6 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
    wp_enqueue_style( 'isr-custom-css', get_stylesheet_directory_uri() . '/app.css', array(), get_the_time() );
+   wp_enqueue_style( 'isr-font-awesome', get_stylesheet_directory_uri() . '/font-awesome.css', array(), get_the_time() );
    wp_enqueue_script( 'isr-custom-js', get_stylesheet_directory_uri() . '/app.js', array(), get_the_time(), true );
 }
